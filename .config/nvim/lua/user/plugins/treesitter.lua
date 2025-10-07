7bf3f3023b6dc312534552ef76a753e90972fd0e
Slawomir Kuzniar <slawomir.kuzniar@gmail.com>
2025-10-07 17:19:34 -0400

    Minor clenup.

--- .config/nvim/lua/user/plugins/treesitter.lua
+++ .config/nvim/lua/user/plugins/treesitter.lua
@@ -2,4 +2,13 @@ return
 {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
+    config = function ()
+        local configs = require("nvim-treesitter.configs")
+        configs.setup({
+            ensure_installed = { "lua", "markdown", "markdown_inline", "yaml" },
+            sync_install = false,
+            highlight = { enable = true },
+            indent = { enable = true },
+        })
+    end,
 }
